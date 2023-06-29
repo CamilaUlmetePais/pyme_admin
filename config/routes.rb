a51fc51da33b35299f1157ddcd3b23d6e0ff3570
Camila Ulmete Pais <33607465+CamilaUlmetePais@users.noreply.github.com>
2023-06-29 19:56:59 -0300

    devise installation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'inflows#index'
   resources :notifications, :outflows, :suppliers, :supply_product_links
