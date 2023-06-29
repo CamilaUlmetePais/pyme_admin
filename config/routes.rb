b0bd551ad8a2357a223a9974afcc249a52488bea
Camila Ulmete Pais <33607465+CamilaUlmetePais@users.noreply.github.com>
2023-06-29 19:36:46 -0300

    initial commit: config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'inflows#index'
+  resources :notifications, :outflows, :suppliers, :supply_product_links
+  resources :inflows do
+    collection do
+      get '/expand/:id', to: 'inflows#expand', as: :expand
+      post 'add_items/:id', to: 'inflows#add_items', as: :add_items
+    end
+  end
+  resources :products, :supplies do
+    collection do
+      get 'mass_stock'
+      post 'mass_stock_update'
+    end
+  end
+  get 'statistics', to: 'pages#statistics', as: :statistics
+  get 'take', to: 'pages#take', as: :take
 end
