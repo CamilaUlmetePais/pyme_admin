24515fa9b3d9fc618fe89823cfe6cadb78720047
CamilaUlmetePais <camilaulmetepais@gmail.com>
2024-06-21 12:30:48 -0300

    config: fixed missing default value in user role

--- db/migrate/20230727204745_add_details_to_users.rb
+++ db/migrate/20230727204745_add_details_to_users.rb
@@ -1,6 +1,6 @@
 class AddDetailsToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :name, :string
-    add_column :users, :role, :integer, default:
+    add_column :users, :role, :integer, default: 1
   end
 end
